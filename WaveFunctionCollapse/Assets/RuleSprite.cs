112be71a6db542bcb31ffe4dc2efd49138b0a094
Daniel Hilo Hansen <55060805+KYNIKER@users.noreply.github.com>
2023-02-15 07:50:59 +0100

    Added RuleSprite

---
+++ WaveFunctionCollapse/Assets/RuleSprite.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuleSprite : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public List<RuleSprite> Self = new();
+
+    public List<RuleSprite> Proximity = new();
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
